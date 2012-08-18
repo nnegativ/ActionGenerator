abc585494020c7fbc463b8149174c6a040ca395f
Marek Rogoziński <nnegativ@gmail.com>
2012-08-18 22:17:15 +0200

    ComplexEvent implementation and sink example using Solr

---
+++ ag-player/src/main/java/com/sematext/ag/event/ComplexEvent.java
@@ -0,0 +1,16 @@
+package com.sematext.ag.event;
+
+import pl.solr.dm.types.ObjectDataType;
+
+public class ComplexEvent extends Event {
+	private ObjectDataType object;
+	
+	public ComplexEvent(final ObjectDataType object) {
+		this.object = object;
+	}
+
+	public ObjectDataType getObject() {
+		return object;
+	}
+
+}
